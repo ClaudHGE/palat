dca66c7e2fdcda373a04f5ed5cf47e796f61c6a2
Claudia <143983702+ClaudHGE@users.noreply.github.com>
2024-09-23 21:56:29 +1000

    Initial commit

--- R/install_if_needed.R
+++ R/install_if_needed.R
@@ -1,3 +1,20 @@
+#' Install a package only if it hasn't before. Else/then load it
+#'
+#' @param pkg the name of the package in quotation marks
+#'
+#' @return NULL Package loaded
+#' @export
+#'
+#' @examples
+#' #' install_if_needed("dplyr")
+#'
+#' # List of packages you want to ensure are installed
+#' packages <- c("devtools", "BiocManager", "dartRverse", "leaflet")
+#' #' # Install and load each package
+#' for (pkg in packages) {
+#'   install_if_needed(pkg)
+#' }
+#'
 install_if_needed <- function(pkg) {
   if (!require(pkg, character.only = TRUE)) {
     install.packages(pkg, dependencies = TRUE, quiet = TRUE)
